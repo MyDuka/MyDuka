7533f98aec11aaf86ae2f4602134fb5f496bbdd9
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-16 04:30:39 -0700

    add crud methods

--- api/app/controllers/products_controller.rb
+++ api/app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+
+
+
+    def index 
+        products = Product.all
+
+        render json: products, status: :ok
+    end
+
+
+    def create 
+        product = Product.create(product_params)
+        if product.valid? 
+            render json: product, status: :created
+        else 
+            render json: {message: "Product not created"}, status: :unprocessable_entity
+        end
+    end
+
+
+
 end
