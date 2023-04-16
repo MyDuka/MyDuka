549afc915d7a0c0c66177a04f88745434b590e0e
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-16 12:21:50 -0700

    add store id

--- api/app/controllers/products_controller.rb
+++ api/app/controllers/products_controller.rb
@@ -9,13 +9,45 @@ class ProductsController < ApplicationController
     end
 
 
-    def create 
-        product = Product.create(product_params)
-        if product.valid? 
+    def add_product 
+        store = Store.find_by(id: params[:id])
+        
+        if store
+            product = store.products.create(product_params) 
             render json: product, status: :created
         else 
             render json: {message: "Product not created"}, status: :unprocessable_entity
         end
+    end
+
+
+    def show
+        product = Product.find_by(id: params[:id])
+        if product
+            render json: product, status: :ok
+        else 
+            render json: {message: "Not Found", product.errors}, status: :unprocessable_entity
+        end
+    end
+
+
+    def update
+        product = Product.find_by(id: params[:id])
+        if product
+            product = Product.update(product_params)
+            render json: {product, info: "Updated succesfully"},status: :ok
+
+        else  
+            render json: {message: "Not updated"}, status: :unprocessable_entity
+        end
+
+    end
+
+    def destroy
+        product = Product.find(params[:id])
+        product.destroy
+        header :no_content
+
     end
 
 
