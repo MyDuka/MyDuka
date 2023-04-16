acffbf7a1885ae1eed4c70df11fa2a066ca8272e
cindymuthoni <cindymuthoni14@gmail.com>
2023-04-16 16:31:42 +0300

    added controllers to admin

--- api/app/controllers/admins_controller.rb
+++ api/app/controllers/admins_controller.rb
@@ -1,2 +1,34 @@
-class AdminsController < ApplicationController
+class AdminsController < ApplicationController    
+    before_action :authentitate_admin!, except:[:index, :show]
+
+    #GET/admin
+    def index
+        admin = Admin.All 
+        render json: admins
+    end
+    #GET/admin/:id
+        def show
+      admin = Admin.find_by(id: params[:id])
+      if admin
+        render json: recipe
+      end
+    end
+  
+    # POST /admins
+    def create
+      recipe = Admin.new(admin_params)
+      if admin.save
+        render json: admin
+      else
+
 end
+
+render json: { errors: admin.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /admin/:id
+    def destroy
+      admin = Admin.find(params[:id])
+      admin.destroy
+      render json: { message: "Admin deleted" }
