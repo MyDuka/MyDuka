6190e9251baffbc65dadd7f7cc5bd80f9d3b3f79
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-16 12:22:17 -0700

    add relation

--- api/app/models/admin.rb
+++ api/app/models/admin.rb
@@ -4,6 +4,8 @@ class Admin < ApplicationRecord
   enum :status [:ACTIVE, :DEACTIVATED]
 
   belongs_to :merchant
+  has_many :clerks
+  has_many :store_admins
 
   validates :email, presence: true
 
