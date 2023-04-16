b9a0add943e4ff469831c446e7cbc732c72c33fa
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-16 04:30:11 -0700

    add admin, merchant  methods

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+
+
+    # merchants methods
+
+    def merchant 
+        merchant.find(session[:merchant_id].to_i)
+    end
+
+    def merchant_params
+        params.permit(:username, :password, :email)
+    end
+
+    # clerk methods
+
+    def clerk_params 
+        params.permit(:username, :password, :email)
+    end
+
+    # admin methods
+
+    def admin_params
+        params.permit(:username, :password, :email) 
+    end
+
+
 end
