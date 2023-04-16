682eaf3e15768f5a497aaf6c31f9af8b8b177d05
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-16 04:32:18 -0700

    add login, logout routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,10 +1,39 @@
 Rails.application.routes.draw do
   resources :products
   resources :clerks
-  resources :admins
+  resources :admins, only: [:show, :update, :delete]
   resources :merchants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  # Admin routes
+
+  post '/register', to: "products#register"
+  
+  post '/admins/login', to: "sessions#admin_login"
+
+  # merchant routes
+
+  post '/merchants/login', to: "sessions#merchant_login"
+
+  # clerk routes
+
+  post 'clerks/login', to: "sessions#clerk_login"
+
+
+  # logout
+
+  delete '/logout', to: "sessions#logout"
+
+  get '/login/status', to: "sessions#login_status"
+
+
+
+
+
+
+
 end
