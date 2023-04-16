2dd43912e0461d09af290c5b093f13c97b82d32a
the-datageek <moraaf770@gmail.com>
2023-04-16 23:48:41 +0300

    Added a sidebar for the clerks dashboard

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import {UilSignOutAlt} from '@iconscout/react-unicons';
+import { SidebarData } from '../Data/Data';
+import './Sidebar.css'
+
+
+const Sidebar = () => {
+
+  // making only one item active in the sidebar
+  const [selected, setSelected] = useState(0)
+
+  return (
+    <div className='Clerk'>
+        <div className='Clerk-glass'>
+        <div className='Sidebar'>
+      {/* Logo */}
+      <div>
+        <label>MyDuka</label>
+      </div>
+      {/* Menu */}
+      <div className='menu'>
+        {SidebarData.map((item, index)=>{
+          return(
+            <div className={selected===index?'menuItem active': 'menuItem'}
+              key={index}
+              onClick={()=>setSelected(index)}
+            >
+              <item.icon className="icon"/>
+              <span>
+                {item.heading}
+              </span>
+            </div>
+          )
+        })}
+        
+        <div className='menuItem'>
+          <UilSignOutAlt />
+
+        </div>
+
+
+      </div>
+             
+    </div>
+
+    </div>
+      
+    </div>
+   
+  
+  )
+}
+
+export default Sidebar
