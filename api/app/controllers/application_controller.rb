522dee197417c8b1c08818eef591c3200001cc03
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-16 12:21:33 -0700

    add params

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -12,10 +12,18 @@ class ApplicationController < ActionController::API
         params.permit(:username, :password, :email)
     end
 
+    def merchant_authorization 
+        render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :merchant_id
+    end
+
     # clerk methods
 
     def clerk_params 
         params.permit(:username, :password, :email)
+    end
+
+    def clerk_authorize 
+        render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :clerk_id
     end
 
     # admin methods
@@ -24,7 +32,29 @@ class ApplicationController < ActionController::API
         params.permit(:username, :password, :email) 
     end
 
-    # products method
+    def admin_authorize 
+        render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :admin_id
+
+    end
+
+    # products methods
+
+    def products_params
+        params.permit(:name, :type, :buying_price, :selling_price, :supplier, :image)
+    end
+
+
+    # store methods
+
+    def store_params
+        params.permit(:name, :address)
+    end
+
+    #  received items
+
+    def ri_params
+        params.permit(:received, :payment_status, :stocked, :spoilt) 
+    end
 
 
 end
