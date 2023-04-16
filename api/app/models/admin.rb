30a47fde335f448b57e932f5e92b1a711e4aac21
cindymuthoni <cindymuthoni14@gmail.com>
2023-04-16 16:56:07 +0300

    added model

--- api/app/models/admin.rb
+++ api/app/models/admin.rb
@@ -1,2 +1,5 @@
 class Admin < ApplicationRecord
+    belongs_to :merchant
+    has_many :clerks, dependent: :destroy 
+    has_many :admin_store, dependent: :destroy
 end
--- api/app/serializers/admin_serializer.rb
+++ api/app/serializers/admin_serializer.rb
@@ -1,3 +1,6 @@
 class AdminSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :password_digest, :status
+  belongs_to :merchant
+  has_many :clerks, dependent: :destroy 
+  has_many :admin_store, dependent: :destroy
 end
