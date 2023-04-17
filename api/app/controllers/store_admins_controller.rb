c2e9ebcf96e4acd4125390728740ddc1a793b9a2
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-17 12:02:37 -0700

    add adnin store methods

--- api/app/controllers/store_admins_controller.rb
+++ api/app/controllers/store_admins_controller.rb
@@ -1,2 +1,40 @@
 class StoreAdminsController < ApplicationController
+
+
+    def create 
+        storeAdmn = StoreAdmin.create(storeA_params)
+        render json: storeAdmn, status: :created
+    end
+
+    def index 
+        StoreA = StoreAdmin.all  
+        render json: storeA, status: :ok
+    end
+
+    def destroy 
+        StoreAdmin.find(param[:id]).destroy
+        head :no_content
+    end
+
+    def show 
+        storeA = StoreAdmin.find_by(id: params[id])
+        if storeA 
+            render json: storeA, status: :ok
+        else 
+            render json: {message: "Not found"}, status: :not_found
+        end
+    end
+
+    def update 
+        merchant = Merchant.find(session[:merchant_id]) 
+        store = merchant.stores.find_by(id: params[:id])
+        if store 
+            render json: {message: "updated"}, status: :ok 
+        else  
+            renser json: {message: unprocessable_entity}
+        end
+    end
+    
+
+
 end
