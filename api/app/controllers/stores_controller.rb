405fc5e486bf6b3bc03514a852a58f47b702c8dd
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-17 12:03:04 -0700

    add store methods

--- api/app/controllers/stores_controller.rb
+++ api/app/controllers/stores_controller.rb
@@ -1,2 +1,47 @@
 class StoresController < ApplicationController
+
+
+    def index 
+        store = Store.all 
+        render json; store, status: :ok
+    end
+
+    def show 
+        store = Store.find_by(id: params[:id])
+        if store 
+            render json: store, status: :ok 
+        else   
+            render json: {message: "not found"}, status: :not_found
+        end
+    end
+
+    def add_store
+        merchant = Merchant.find_by(id: sessions[:merchant_id])
+
+        if merchant
+            store = merchant.stores.create(store_params)
+            render json: store, status: :created
+        else  
+            render json:{message: "Not Created"}, status: :unprocessable_entity
+        end
+    end
+
+
+    def update 
+        merchant = Merchant.find_by(id: sessions[:merchant_id])
+        if merchant  
+            store = merchant.store.update(store_params)
+            render json: store, status: :ok
+        else  
+            render json: {message: "not updated"}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        Store.find(params[id]).destroy
+        head :no_content
+    end
+
+
+
 end
