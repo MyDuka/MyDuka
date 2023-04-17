06f9cfa345551c52ed641405539a0ad49a35f737
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-17 12:01:20 -0700

    add merchant methods

--- api/app/controllers/merchants_controller.rb
+++ api/app/controllers/merchants_controller.rb
@@ -1,4 +1,51 @@
 class MerchantsController < ApplicationController
 
 
+    def index 
+        merchant = Merchant.all 
+        render json: merchant, status: :ok
+    end
+
+    def create 
+        merchant = Merchant.create(merchant_param)
+        if merchant.valid?
+            render json: merchant, status: :created 
+        else   
+            render json: {message: "unprocessable"}, status: :unprocessable_entity
+        end
+        
+    end
+
+    def show 
+        merchant = Merchant.find_by(id: params[:id])
+        if merchant 
+            render json: merchant, status: :ok 
+        else  
+            render json: {message: "not found"}, status: :not_found
+        end
+    end 
+
+
+    def update 
+        merchant = Merchant.find_by(id: sessions[:merchant_id])
+        if merchant
+            merchant.update(merchant_params) 
+            render json: merchant, status: :ok
+        else  
+            render json: {message: "unprocessable"}, status: :unprocessable_entity
+        end
+    end
+
+
+    def destroy 
+        Merchant.find(sessions[:params[id]])
+    end
+
+    private 
+
+    def merchant_param
+        params.permit(:username, :password, :email)
+    end
+
+
 end
