bcc751ff7ce4e134e0c723fdf2e9672e62248640
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-18 09:45:46 -0700

    fix add item

--- api/app/controllers/received_items_controller.rb
+++ api/app/controllers/received_items_controller.rb
@@ -10,14 +10,14 @@ class ReceivedItemsController < ApplicationController
         if ri 
             render json: ri, status: :ok
         else  
-            render json: {message: "not found", ri.errors}, status: :not_found
+            render json: {message: "not found"}, status: :not_found
         end
     end
 
     def ri_add
-        product = Product.find_by(:id params[:id])
+        product = Product.find_by(id: params[:id])
         if product 
-            ri = product.recieved_items.create(ri_params)
+            ri = product.received_items.create(ri_params)
             render json: ri, status: :created
         else  
             render json: {message: "not succesfull"}, status: :unprocessable_entity
@@ -25,14 +25,23 @@ class ReceivedItemsController < ApplicationController
     end
 
     def update 
-        product = Product.find_by(:id params[:id])
+        product = Product.find_by(id: params[:id])
         if product 
-            ri = product.recieved_items.update(ri_params)
+            ri = product.received_items.update(ri_params)
             render json: ri, status: :created
         else  
             render json: {message: "not succesfull"}, status: :unprocessable_entity
         end
+    end
 
+    def payment_status 
+        product = Product.find_by(id: params[:id])
+        if product 
+            ri = product.received_items.update(ri_params)
+            render json: ri, status: :created
+        else  
+            render json: {message: "not succesfull"}, status: :unprocessable_entity
+        end
     end
 
     private 
