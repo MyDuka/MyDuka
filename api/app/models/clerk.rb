d0f06249f5819fb90d88fb6e12f0bf928fa100af
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-18 00:26:05 -0700

    add relation

--- api/app/models/clerk.rb
+++ api/app/models/clerk.rb
@@ -1,10 +1,10 @@
 class Clerk < ApplicationRecord
-
     has_secure_password
 
+    enum status: [:ACTIVE, :DEACTIVATED]
+
     belongs_to :admin
-    has_many :product_clerks
-    has_many 
+    has_many :product_clerks 
 
     validates :email, presence: true
 
