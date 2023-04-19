cb1017818d29e868daeab5453a5c9b544322a942
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-19 14:09:25 -0700

    add form

---
+++ client/src/components/auth/ClerkLogin.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './MerchantAuth.css'
+import { Link } from "react-router-dom";
+
+
+export default function ClerkLogin(){
+
+    return(
+        <>
+       <div className="login-wrap">
+            
+
+            <div className="login-form">
+        
+            <form className="sign-in-htm"  >
+				<div className="group">
+					<label for="user" className="label">Username</label>
+					<input id="user" type="text" className="input"  />
+				</div>
+				<div className="group">
+					<label for="pass" className="label">Password</label>
+					<input id="pass" type="password" className="input" data-type="password" />
+				</div>
+			
+				<div className="group">
+					<input type="submit" className="button" value="Sign In" />
+				</div>
+				<div className="hr"></div>
+				<div className="foot-lnk">
+                    <Link to="/password/reset">Forgot Password?</Link>
+				</div>
+			</form>
+
+            
+            </div>
+            </div>
+        </>
+    )
+}
