8bdda687028e50300b35b7ac379c949ed2258c4d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-20 04:42:07 -0700

    add form

--- client/src/components/auth/MerchantLogin.js
+++ client/src/components/auth/MerchantLogin.js
@@ -1,41 +1,36 @@
 import React from "react";
-import './MerchantAuth.css'
+import './Auth.css'
 import { Link } from "react-router-dom";
 
 export default function MerchantLogin(){
 
     return(
-        <>
-        <div className="login-wrap">
-            <div >
+        <div className="log">
+        <form className="login-form" >
+  <h4>Merchant Login</h4>
 
-            <div className="login-form">
-        
-            <form className="sign-in-htm"  >
-				<div className="group">
-					<label for="user" className="label">Username</label>
-					<input id="user" type="text" className="input"  />
-				</div>
-				<div className="group">
-					<label for="pass" className="label">Password</label>
-					<input id="pass" type="password" className="input" data-type="password" />
-				</div>
-			
-				<div className="group">
-					<input type="submit" className="button" value="Sign In" />
-				</div>
-				<div className="hr"></div>
-				<div className="foot-lnk">
-                    <Link to="/password/reset">Forgot Password?</Link>
-                    <br/>
-                    <Link to="/signup">Sign up</Link>
-				</div>
-			</form>
+  <label for="username" className="label">Username</label>
+  <div className="form-input-material">
+    <input type="text" name="username" id="username" placeholder="Username" autocomplete="off" className="form-control-material" required /> 
+  </div>
 
-            </div>
-            </div>
-            </div>
+  <label for="password" className="label">Password</label>
+  <div className="form-input-material">
+    <input type="password" name="password" id="password" placeholder="Password" autocomplete="off" className="form-control-material" required />
+  </div>
+
+  <button type="submit" className="btn btn-primary btn-ghost">Login</button>
+
+  <div className="foot-lnk">
+        <Link to="/merchant/signup">Register</Link>
+            <br/>
+        <Link to="/password/reset">Forgot Password?</Link>
         
-        </>
+        
+    </div>
+
+</form>
+        
+        </div>
     )
 }
