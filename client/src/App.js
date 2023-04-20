02130267c180df66eb56ac24bfaedd85c2688354
the-datageek <moraaf770@gmail.com>
2023-04-20 14:03:16 +0300

    done with section 3 of landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Landing from './components/Section0/section0';
+import About from './components/Section1/section1';
+import About2 from './components/Section2/section2';
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      {/* <Landing/> */}
+      {/* <About/> */}
+      <About2/>
+
+      /
     </div>
   );
 }
---
+++ client/src/components/Section1/section1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './section1.css'
+
+const About = () => {
+  
+    return (
+        <div className="about-container">
+          <div className="a-l-section">
+            {/* content for left section */}
+          </div>
+          <div className="a-r-section">
+            {/* content for right section */}
+          </div>
+        </div>
+      );
+  
+}
+
+export default About
