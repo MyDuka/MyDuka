1187f108e70c5b076d916af5ede9a812fb2b3019
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-20 04:43:09 -0700

    add form details

--- client/src/components/auth/AdminLogin.js
+++ client/src/components/auth/AdminLogin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './MerchantAuth.css'
+import './Auth.css'
 import { Link } from "react-router-dom";
 
 
@@ -8,36 +8,28 @@ import { Link } from "react-router-dom";
 export default function AdminLogin(){
 
     return(
-        <>
-  <div className="login-wrap">
-            
+        <div className="log">
 
-            <div className="login-form">
-        
-            <form className="sign-in-htm"  >
-				<div className="group">
-					<label for="user" className="label">Username</label>
-					<input id="user" type="text" className="input"  />
-				</div>
-				<div className="group">
-					<label for="pass" className="label">Password</label>
-					<input id="pass" type="password" className="input" data-type="password" />
-				</div>
-			
-				<div className="group">
-					<input type="submit" className="button" value="Sign In" />
-				</div>
-				<div className="hr"></div>
-				<div className="foot-lnk">
-                    <Link to="/password/reset">Forgot Password?</Link>
-\
-				</div>
-			</form>
+<form className="login-form" >
+  
+  <h4>Admin Login</h4>
+  <label for="username" className="label">Username</label>
+  <div className="form-input-material">
+    <input type="text" name="username" id="username" placeholder="Username" autocomplete="off" className="form-control-material" required />
+  </div>
 
-            
-            </div>
-            </div>
+  <label for="password" className="label">Password</label>
+  <div className="form-input-material">
+    <input type="password" name="password" id="password" placeholder="Password" autocomplete="off" className="form-control-material" required />
+  </div>
+  <button type="submit" className="btn btn-primary btn-ghost">Login</button>
 
-        </>
+  <br/>
+    <div className="foot-lnk">
+        <Link to="/password/reset">Forgot Password?</Link>
+    </div>
+</form>
+  
+        </div>
     )
 }
