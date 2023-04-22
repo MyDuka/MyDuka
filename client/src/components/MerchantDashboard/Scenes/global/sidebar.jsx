dd7ba861ac7f81c7eec3080ada4d0f403ddb0158
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-22 11:38:07 -0700

    edit side bar

--- client/src/components/MerchantDashboard/Scenes/global/sidebar.jsx
+++ client/src/components/MerchantDashboard/Scenes/global/sidebar.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
+import {ProSidebar, Menu, MenuItem} from 'react-pro-sidebar'
 import 'react-pro-sidebar/dist/css/styles.css';
-import { Menu, MenuItem} from 'react-pro-sidebar';
-import { ProSidebar } from 'react-pro-sidebar';
 import {Box, IconButton, Typography, useTheme} from '@mui/material';
 import {Link} from "react-router-dom"
 import { tokens } from '../../../../theme';
