820246b97ebc02f14ea9283bb033e18705097a5a
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-22 11:42:16 -0700

    change routes

--- client/src/components/LandingPage/index.jsx
+++ client/src/components/LandingPage/index.jsx
@@ -3,10 +3,11 @@ import Landing from '../Section0/section0'
 import About from '../Section1/section1'
 import About2 from '../Section2/section2'
 import Navbar from './navbar'
+import './Landingpage.css'
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className="home">
         <Navbar/>
         <Landing/>
         <About/>
