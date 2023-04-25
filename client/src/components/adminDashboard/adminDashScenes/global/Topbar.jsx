e8c5c41b324c8c63beaa62ff004db4c2433dda0f
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-25 04:36:19 -0700

    remove links

--- client/src/components/adminDashboard/adminDashScenes/global/Topbar.jsx
+++ client/src/components/adminDashboard/adminDashScenes/global/Topbar.jsx
@@ -6,20 +6,9 @@ import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
-import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+
 import SearchIcon from "@mui/icons-material/Search";
 
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
-import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
-import { RiPencilLine } from "react-icons/ri";
-import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
-
-
-import { FiLogOut} from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 
@@ -36,19 +25,10 @@ const AdminTopbar = () => {
         backgroundColor={colors.primary[400]}
         borderRadius="3px"
       >
-        <IconButton ><Link className="admm" to="/admin/dashbord">< HomeOutlinedIcon /></Link></IconButton>
-        <IconButton><Link className="admm"  to="/admin/team"><PeopleOutlinedIcon /></Link></IconButton>
-        <IconButton><Link className="admm"  to="/admin/products"> <ContactPhoneIcon /></Link></IconButton>
-        <IconButton ><Link className="admm"  to="/admin/form"><PersonAddAltIcon /></Link></IconButton>
-        <IconButton ><Link className="admm"  to="/admin/bar"><BarChartOutlinedIcon /></Link></IconButton>
-        <IconButton ><Link className="admm"  to="/admin/pie"><PieChartOutlineOutlinedIcon /></Link></IconButton>
-        <IconButton><Link className="admm"  to="/admin/line"><TimelineOutlinedIcon /></Link></IconButton>   
-
-{/* 
         <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
         <IconButton type="button" sx={{ p: 1 }}>
           <SearchIcon />
-        </IconButton> */}
+        </IconButton>
       </Box>
 
       {/* ICONS */}
@@ -66,14 +46,8 @@ const AdminTopbar = () => {
         <IconButton>
           <SettingsOutlinedIcon />
         </IconButton>
-        <IconButton>
-          <Link to="/admin/form/update"> <PersonOutlinedIcon /> </Link>
-          
-        </IconButton>
-        <IconButton>
-            <Link to="/"><FiLogOut /></Link>
-        </IconButton>
-
+     
+        
       </Box>
     </Box>
   );
