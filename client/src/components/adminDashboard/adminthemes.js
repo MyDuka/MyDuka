46bc58b53588a727e5410787bb51940dfe6c277a
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-25 04:40:02 -0700

    change color theme

--- client/src/components/adminDashboard/adminthemes.js
+++ client/src/components/adminDashboard/adminthemes.js
@@ -13,14 +13,15 @@ export const tokens = (mode) => ({
           500: "#666666",
           600: "#525252",
           700: "#3d3d3d",
-          800: "#292929",
+          800: "#21202A",
           900: "#141414",
         },
         primary: {
           100: "#d0d1d5",
           200: "#a1a4ab",
           300: "#727681",
-          400: "#1F2A40",
+          // 400: "#1e0127",
+          400: "#0C0D22",
           500: "#141b2d",
           600: "#101624",
           700: "#0c101b",
@@ -28,16 +29,27 @@ export const tokens = (mode) => ({
           900: "#040509",
         },
         greenAccent: {
-          100: "#dbf5ee",
-          200: "#b7ebde",
-          300: "#94e2cd",
-          400: "#70d8bd",
-          500: "#4cceac",
-          600: "#3da58a",
-          700: "#2e7c67",
-          800: "#1e5245",
-          900: "#0f2922",
+          100: "#8b2800",
+          200: "#cc4d00",
+          300: "#ff751a",
+          400: "#ff9966",
+          500: "#ff7b00",
+          600: "#ffcca5",
+          700: "#ffe0b3",
+          800: "#ffe6cc",
+          900: "#fff2e6"
         },
+        orangeAccent:{
+          100: "#8b2800",
+          200: "#cc4d00",
+          300: "#ff751a",
+          400: "#ff9966",
+          500: "#ffb399",
+          600: "#ffcca5",
+          700: "#ffe0b3",
+          800: "#ffe6cc",
+          900: "#fff2e6"
+      },
         redAccent: {
           100: "#f8dcdb",
           200: "#f1b9b7",
@@ -60,8 +72,18 @@ export const tokens = (mode) => ({
           800: "#2a2d64",
           900: "#151632",
         },
-      }
-    : {
+        purpleAccent:{
+          100: "#1e0127",
+          200: "#1D192C",
+          300: "#7d699f",
+          400: "#a58ccd",
+          500: "#c1a2e8",
+          600: "#d3b2e6",
+          700: "#e4c2e4",
+          800: "#f3d2f3",
+          900: "#f8e6f8"
+         },
+        }: {
         grey: {
           100: "#141414",
           200: "#292929",
@@ -78,22 +100,33 @@ export const tokens = (mode) => ({
           200: "#080b12",
           300: "#0c101b",
           400: "#f2f0f0", // manually changed
-          500: "#141b2d",
+          500: "#141c2d",
           600: "#1F2A40",
           700: "#727681",
           800: "#a1a4ab",
           900: "#d0d1d5",
         },
         greenAccent: {
-          100: "#0f2922",
-          200: "#1e5245",
-          300: "#2e7c67",
-          400: "#3da58a",
-          500: "#4cceac",
-          600: "#70d8bd",
-          700: "#94e2cd",
-          800: "#b7ebde",
-          900: "#dbf5ee",
+          100: "#8b2800",
+          200: "#cc4d00",
+          300: "#ff751a",
+          400: "#ff9966",
+          500: "#ff7b00",
+          600: "#ffcca5",
+          700: "#ffe0b3",
+          800: "#ffe6cc",
+          900: "#fff2e6"
+        },
+        orangeAccent:{
+            100: "#8b2800",
+            200: "#cc4d00",
+            300: "#ff751a",
+            400: "#ff9966",
+            500: "#ff7b00",
+            600: "#ffcca5",
+            700: "#ffe0b3",
+            800: "#ffe6cc",
+            900: "#fff2e6"
         },
         redAccent: {
           100: "#2c100f",
@@ -117,6 +150,17 @@ export const tokens = (mode) => ({
           800: "#c3c6fd",
           900: "#e1e2fe",
         },
+        purpleAccent:{
+          100: "#2e2532",
+          200: "#1e0127;",
+          300: "#7d699f",
+          400: "#a58ccd",
+          500: "#c1a2e8",
+          600: "#d3b2e6",
+          700: "#e4c2e4",
+          800: "#f3d2f3",
+          900: "#f8e6f8"
+         },
       }),
 });
 
@@ -133,7 +177,7 @@ export const themeSettings = (mode) => {
               main: colors.primary[500],
             },
             secondary: {
-              main: colors.greenAccent[500],
+              main: colors.orangeAccent[200],
             },
             neutral: {
               dark: colors.grey[700],
@@ -141,8 +185,9 @@ export const themeSettings = (mode) => {
               light: colors.grey[100],
             },
             background: {
-              default: colors.primary[500],
+              default: colors.grey[800],
             },
+          
           }
         : {
             // palette values for light mode
@@ -150,7 +195,7 @@ export const themeSettings = (mode) => {
               main: colors.primary[100],
             },
             secondary: {
-              main: colors.greenAccent[500],
+              main: colors.orangeAccent[200]
             },
             neutral: {
               dark: colors.grey[700],
@@ -160,33 +205,35 @@ export const themeSettings = (mode) => {
             background: {
               default: "#fcfcfc",
             },
+          
           }),
     },
     typography: {
-      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontFamily: ["Poppins", "sans-serif"].join(","),
       fontSize: 12,
       h1: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
+
         fontSize: 40,
       },
       h2: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 32,
       },
       h3: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 24,
       },
       h4: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 20,
       },
       h5: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 16,
       },
       h6: {
-        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontFamily: ["Poppins", "sans-serif"].join(","),
         fontSize: 14,
       },
     },
