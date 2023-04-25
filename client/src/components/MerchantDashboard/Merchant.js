2ca4b568e90e9d1b333ec3109a3bf642f347a524
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-25 04:38:26 -0700

    add routes

--- client/src/components/MerchantDashboard/Merchant.js
+++ client/src/components/MerchantDashboard/Merchant.js
@@ -1,15 +1,10 @@
 import {useState} from "react"
 import {ColorModeContext, useMode} from  '../../theme'
 import {CssBaseline, ThemeProvider } from '@mui/material';
-import {Routes, Route} from "react-router-dom"
+import {Outlet} from "react-router-dom"
 import MerchSidebar from "./Scenes/global/sidebar";
 import Topbar from './Scenes/global/topbar';
-import Dashboard from "./Scenes/dashboard/index.jsx";
-import Team from "./Scenes/team/index.jsx";
-import Form from "./Scenes/form";
-import Bar from "./Scenes/bar/index.jsx";
-import Pie from "./Scenes/pie/index.jsx";
-import Line from "./Scenes/line/index.jsx";
+
 
 
 function Merchant() {
@@ -33,17 +28,9 @@ function Merchant() {
                 <MerchSidebar isSidebar={isSidebar}/>
                 <main className='content'>
                     <Topbar setIsSidebar={setisSidebar}/>
-                       {/* <LandingpageNavbar/>  */}
                        
-                    <Routes>
-                        <Route path="/" element={<Dashboard />} />
-                        <Route path="/team" element={<Team />} />
-                        <Route path="/form" element={<Form />} />
-                        <Route path="/bar" element={<Bar/>} /> 
-                        <Route path="/pie" element={<Pie/>} /> 
-                        <Route path="/line" element={<Line/>} /> 
-                    </Routes>
-                   
+                    <Outlet/>   
+                
                 </main>
             </div>
     </ThemeProvider>
