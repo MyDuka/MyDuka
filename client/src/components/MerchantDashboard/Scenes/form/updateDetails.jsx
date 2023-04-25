0f6730df0aa9bb45a7d4b8c96f63ccdbdf5d352b
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-25 04:37:23 -0700

    merchant detail update form

---
+++ client/src/components/MerchantDashboard/Scenes/form/updateDetails.jsx
@@ -0,0 +1,128 @@
+import { Box, Button, TextField } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../../Header";
+import { useState } from "react";
+import axios from 'axios';
+import {Typography} from "@mui/material";
+
+const MerchantUpdateForm = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [contact, setContact] = useState("")
+  const [address, setAddress] = useState("")
+  const [image, setImage] = useState("")
+  // const [lastName, setLastName] = useState("")
+
+  const merchant_id = localStorage.getItem('merchant_id')
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    axios.put(`http://localhost:3000/merchants/${merchant_id}`, {
+  username: username,
+  contact: contact,
+  password: password,
+  address: address,
+  image: image,
+
+})
+.then(response => {
+  console.log(response);
+  // do something with the response, such as saving the user information
+  if (response.status === 200) {
+    console.log(response);
+    return response
+  }
+})
+
+
+  
+    .catch(error => {
+      console.log(error);
+ 
+    });
+  }
+
+
+  return (
+    <Box m="20px">
+      <Header title="Profile" subtitle="Update your Profile" />
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+              }}
+            >
+            <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="image urrl"
+                onChange={(e)=>setImage(e.target.value)}
+                value={image}
+                name="image"
+                // error={!!touched.contact && !!errors.contact}
+                // helperText={touched.contact && errors.contact}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="username"
+                onChange={(e)=> setUsername(e.target.value)}
+                value={username}
+                name="username"
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Contact"
+                onChange={(e)=>setContact(e.target.value)}
+                value={contact}
+                name="contact"
+                // error={!!touched.contact && !!errors.contact}
+                // helperText={touched.contact && errors.contact}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address"
+                onChange={(e)=> setAddress(e.target.value)}
+                value={address}
+                name="address"
+                sx={{ gridColumn: "span 4" }}
+              />
+              <Typography>Change Password</Typography>
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="change password"
+                onChange={(e)=>setPassword(e.target.value)}
+                value={password}
+                name="changePassword"
+                // error={!!touched.address2 && !!errors.address2}
+                // helperText={touched.address2 && errors.address2}
+                sx={{ gridColumn: "span 4" }}
+              />
+            </Box>
+            <Box display="flex" justifyContent="end" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                save
+              </Button>
+            </Box>
+          </form>
+    </Box>
+  );
+};
+
+export default MerchantUpdateForm;
--- client/src/components/MerchantDashboard/Scenes/global/sidebar.jsx
+++ client/src/components/MerchantDashboard/Scenes/global/sidebar.jsx
@@ -1,232 +1,105 @@
-import React from 'react'
-import {useState} from 'react'
-import {ProSidebar, Menu, MenuItem} from 'react-pro-sidebar'
-import 'react-pro-sidebar/dist/css/styles.css';
-import {Box, IconButton, Typography, useTheme} from '@mui/material';
-import {Link} from "react-router-dom"
-import { tokens } from '../../../../theme';
+import { useState } from "react";
+import { Box, IconButton, Typography} from "@mui/material";
+import { Link, Navigate } from "react-router-dom";
+
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
 import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
-import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import PersonOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import MapIcon from '@mui/icons-material/Map';
 
 
-const Item = ({ title, to, icon, selected, setSelected }) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
+
+
+const MerchantSidebar = () => {
+
+  const [img,setImg] = useState("https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png")
+
+  function handleLogout(){
+    fetch("/admin/logout",{
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    localStorage.removeItem('admin_id')
+    return <Navigate to="/" />
+  }
+
   return (
-    <MenuItem
-      active={selected === title}
-      style={{
-        color: colors.grey[100],
-      }}
-      onClick={() => setSelected(title)}
-      icon={icon}
-    >
-      <Typography>{title}</Typography>
-      <Link to={to} />
-    </MenuItem>
-  );
-};
 
-const MerchSidebar = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const [isCollapsed, setIsCollapsed] = useState(false);
-  const [selected, setSelected] = useState("Dashboard");
-
-  return (
-    <Box
-      sx={{
-        "& .pro-sidebar-inner": {
-          background: `${colors.primary[400]} !important`,
-        },
-        "& .pro-icon-wrapper": {
-          backgroundColor: "transparent !important",
-        },
-        "& .pro-inner-item": {
-          padding: "5px 35px 5px 20px !important",
-        },
-        "& .pro-inner-item:hover": {
-          color: "#ff7b00 !important",
-        },
-        "& .pro-menu-item.active": {
-          color: "#ff7b00 !important",
-        },
-      }}
-    >
-      <ProSidebar collapsed={isCollapsed}>
-        <Menu iconShape="square">
-          {/* LOGO AND MENU ICON */}
-          <MenuItem
-            onClick={() => setIsCollapsed(!isCollapsed)}
-            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
-            style={{
-              margin: "10px 0 20px 0",
-              color: colors.grey[100],
-            }}
-          >
-            {!isCollapsed && (
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                ml="15px"
-              >
-                {/* App Logo */}
-                <Typography variant="h3" color={colors.grey[100]} fontWeight="bold">
-                  MyDuka
-                </Typography>
-                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
-                  <MenuOutlinedIcon />
-                </IconButton>
-              </Box>
-            )}
-          </MenuItem>
-          {/* User */}
-
-          {!isCollapsed && (
+    <>
+    <nav className="nav">
+        <div> 
+            <Typography className="nav_logo"> 
+            <i><AddBusinessIcon/></i> 
+            <span className="nav_logo-name">MyDuka</span> </Typography>
+          
+            <br/>
+            <br/>
             <Box mb="25px">
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <img
-                  alt="profile-user"
-                  width="100px"
-                  height="100px"
-                  src={`https://i.pinimg.com/564x/4b/0f/2b/4b0f2b65933a5a2320e564caa877abd1.jpg`}
-                  style={{ cursor: "pointer", borderRadius: "50%" }}
-                />
-              </Box>
-              <Box textAlign="center">
-                <Typography
-                  variant="h4"
-                  color={colors.grey[100]}
-                  fontWeight="bold"
-                  sx={{ m: "10px 0 0 0" }}
-                >
-                  Jane Doe
-                </Typography>
-                <Typography variant="h5" color={colors.orangeAccent[500]}>
-                  Super Merchant
-                </Typography>
-              </Box>
-            </Box>
-          )}
-
-          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
-            <Item
-              title="Dashboard"
-              to="/"
-              icon={<HomeOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <img
+              alt="profile-user"
+              width="100px"
+              height="100px"
+              src={img}
+              style={{ cursor: "pointer", borderRadius: "50%" }}
             />
-
+     
+          </Box>
+          <Box textAlign="center">
             <Typography
-              variant="h5"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
+              variant="h4"
+              fontWeight="bold"
+              sx={{ m: "10px 0 0 0" }}
             >
-              Data
+             Mwenyewe
             </Typography>
-            <Item
-              title="Manage Team"
-              to="/team"
-              icon={<PeopleOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            {/* <Item
-              title="Contacts Information"
-              to="/contacts"
-              icon={<ContactsOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            /> */}
-            {/* <Item
-              title="Invoices Balances"
-              to="/invoices"
-              icon={<ReceiptOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            /> */}
-
-            <Typography
-              variant="h5"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
-              Pages
+            <Typography variant="h5" >
+              Merchant
             </Typography>
-            <Item
-              title="Profile Form"
-              to="/form"
-              icon={<PersonOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            {/* <Item
-              title="Calendar"
-              to="/calendar"
-              icon={<CalendarTodayOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="FAQ Page"
-              to="/faq"
-              icon={<HelpOutlineOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            /> */}
-
-            <Typography
-              variant="h5"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
+          </Box>
+        </Box>
+            <div className="nav_list"> 
+            <Link to="/merchant" className="nav_link active"> <i> <HomeOutlinedIcon/></i> <span className="nav_name">Dashboard</span> </Link> 
+            <Link to="/merchant/team" className="nav_link"><i><PeopleOutlinedIcon/></i>  <span className="nav_name">Admins</span> </Link> 
+            <Link to="/merchant/stores" className="nav_link"> <i> <InventoryIcon/> </i> <span className="nav_name">Stores</span> </Link> 
+            <Link to="/merchant/products" className="nav_link"> <i> <InventoryIcon/> </i> <span className="nav_name">Products</span> </Link> 
+            <Link to="/merchant/form" className="nav_link"> <i> <PersonAddAltIcon/> </i> <span className="nav_name">Add Admin</span> </Link> 
+            <Link to="/merchant/calendar" className="nav_link"> <i> <CalendarMonthIcon/> </i> <span className="nav_name">Calendar</span> </Link> 
+            <br/>
+            <Typography variant="h5" >
               Charts
             </Typography>
-            <Item
-              title="Bar Chart"
-              to="/bar"
-              icon={<BarChartOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Pie Chart"
-              to="/pie"
-              icon={<PieChartOutlineOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Line Chart"
-              to="/line"
-              icon={<TimelineOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            <Item
-              title="Logout"
-              to="/geography"
-              icon={<ExitToAppIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-            {/* <ExitToAppIcon /> */}
-          </Box>
-        </Menu>
-      </ProSidebar>
-    </Box>
+            <Link to="/merchant/bar" className="nav_link"> <i> <BarChartOutlinedIcon /> </i> <span className="nav_name">Bar</span> </Link>
+            <Link to="/merchant/pie" className="nav_link"> <i><PieChartOutlineOutlinedIcon /></i> <span class="nav_name">Pie</span> </Link>
+            <Link to="/merchant/line" className="nav_link"> <i> <TimelineOutlinedIcon /> </i> <span className="nav_name">Line</span> </Link>
+            <Link to="/merchant/geography" className="nav_link"> <i> <MapIcon /> </i> <span className="nav_name">Map</span> </Link>
+             </div>
+             <br/>
+             <Typography variant="h5" >
+              Profile
+            </Typography> 
+            <Link to="/merchant/form/update" className="nav_link"> <i> <PersonOutlinedIcon />  </i> <span className="nav_name">Edit</span> </Link>
+
+        </div> 
+       
+        <IconButton onClick={handleLogout}  className="nav_link"> <i> <LogoutIcon/> </i> <span className="so">SignOut</span> </IconButton>
+    </nav>
+    </>
+
+
   );
 };
-export default MerchSidebar
+
+export default MerchantSidebar;
