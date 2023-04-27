cfcd14bbf59d8f0684ad49bdf848ddced1c8965d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-26 22:17:50 -0700

    add image, contact, address columns

--- api/db/migrate/20230412130221_create_clerks.rb
+++ api/db/migrate/20230412130221_create_clerks.rb
@@ -4,7 +4,10 @@ class CreateClerks < ActiveRecord::Migration[7.0]
       t.string :username
       t.string :email
       t.string :password_digest, default: "12345"
-      t.integer :status, default: 0
+      t.integer :access, default: 0
+      t.string :image
+      t.string :contact
+      t.string :address
       t.belongs_to :admin, null: false, foreign_key: true
 
       t.timestamps
