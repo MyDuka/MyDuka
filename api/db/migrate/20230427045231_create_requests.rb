4c8a39d976647cd51ea07a4a3651212507cad6ef
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-26 22:18:08 -0700

    add request table

---
+++ api/db/migrate/20230427045231_create_requests.rb
@@ -0,0 +1,11 @@
+class CreateRequests < ActiveRecord::Migration[7.0]
+  def change
+    create_table :requests do |t|
+      t.string :product
+      t.integer :quantity
+      t.integer :state, default: 0
+
+      t.timestamps
+    end
+  end
+end
