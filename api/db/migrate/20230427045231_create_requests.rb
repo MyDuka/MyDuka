41ac50d4bcb1a37abd9af7296955cb84f3657175
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-28 03:26:49 -0700

    creare request model

--- api/db/migrate/20230427045231_create_requests.rb
+++ api/db/migrate/20230427045231_create_requests.rb
@@ -2,6 +2,7 @@ class CreateRequests < ActiveRecord::Migration[7.0]
   def change
     create_table :requests do |t|
       t.string :product
+      t.string :supplier
       t.integer :quantity
       t.integer :state, default: 0
 
