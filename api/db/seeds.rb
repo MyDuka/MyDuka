3b213fb378ddbae75eb822203fdccf26a66be6b3
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-28 03:25:08 -0700

    add seed data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -6,8 +6,38 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "💤..seed ninja...🥷"
 
 6.times do 
-    Store.create()
+    product = Product.create(
+        "name": Faker::Beer.name ,
+        "category": Faker::Beer.brand,
+        "supplier": "Brew Masters 254",
+        "buying_price":  rand(3894.37..6562.37),
+        "selling_price":  rand(6563.98..7846.78),
+        "store_id": 3
+    )
+
+    rand(3..7).times do
+        ReceivedItem.create(
+        "received":  rand(56..258),
+        "payment_status": rand(0..1),
+        "stocked":  rand(678..789),
+        "spoilt": rand(67..832),
+        "product_id": product.id
+    )
+    end
+
+    3.times do 
+        Request.create(
+            "product": Faker::Beer.name,
+            "supplier": "EABL",
+            "quantity": rand(67..367),
+            "state": rand(0..1)
+        )
+    end
 
 end
+
+
+puts "🤾 . dusted. . 😅"
